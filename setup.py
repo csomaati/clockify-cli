39d456e82882a44233a069f4210bd07cf68d1f07
Attila Csoma <acsoma@leannet.eu>
2020-02-02 21:23:42 +0100

    Rewrite setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,18 @@ setup(
     name='clockify_cli',
     version='0.10',
     py_modules=['clockify_cli'],
-    author='Theodore Hu',
-    url='https://github.com/t5/clockify-cli',
+    author='Attila Csoma',
+    url='https://github.com/csomaati/clockify-cli',
     install_requires=[
         'click>=7.0',
         'certifi==2018.8.13',
         'chardet==3.0.4',
-        'click==6.7',
         'idna==2.7',
         'requests>=2.20.0',
         'urllib3==1.23',
     ],
     entry_points='''
         [console_scripts]
-        clockify=clockify_cli.clockify_cli:main
+        clockify=clockify_cli.cli:main
    ''', 
 )
